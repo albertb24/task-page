0defb4f3722562236c1648b5982283591338e482
albertb24 <albert.karol.bak@gmail.com>
2021-10-29 14:44:48 +0200

    firebase configuration file
    firebase configuration file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component.jsx';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Header/>
+    <Switch>
+      <Route exact path='/' component = {HomePage} />
+      <Route exact path='/shop' component = {ShopPage} />
+      <Route exact path='/signin' component = {SignInAndSignUpPage} />
+    </Switch>
     </div>
   );
 }
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import firebase/firestore;
+import firebase/auth;
+
+const config = {
+    apiKey: "AIzaSyAAQqRG4Qh1es9IXXDnnvaff6q4NjVGtDY",
+    authDomain: "task-page-57f2e.firebaseapp.com",
+    projectId: "task-page-57f2e",
+    storageBucket: "task-page-57f2e.appspot.com",
+    messagingSenderId: "830207799336",
+    appId: "1:830207799336:web:cf413416ffbe8b146f051d",
+    measurementId: "G-6P2PENTWJ6"
+  };
+
+firebase.initializeApp(config);
